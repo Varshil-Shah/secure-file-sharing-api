1941090d0a4540df860a762a7067b7183a46c430
Varshil-Shah <varshilashah09@gmail.com>
2022-06-30 13:58:21 +0530

    :sparkles: Implement a func to encrypt a password

--- models/file-model.js
+++ models/file-model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const fileSchema = new mongoose.Schema(
   {
@@ -27,6 +28,13 @@ const fileSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+fileSchema.pre("save", async function (next) {
+  if (this.password) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
 
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
